ebc5ec758ab0508f6781d35d655962f4f5c2d1c4
Gabriel Brogni Zaccaron <gabrielbrogni@hotmail.com>
2017-12-07 00:11:14 -0200

    first

--- libs/boot.js
+++ libs/boot.js
@@ -1,5 +1,7 @@
 module.exports = app => {
-    app.listen(process.env.OPENSHIFT_NODEJS_PORT || 3000, () => {
+    var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+        ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+    app.listen(port, ip, () => {
         console.log("API iniciada.");
     });
 }
